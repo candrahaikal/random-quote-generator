18cf65ca62456f219b54ae1afbdc9a032555acfb
Candra Haikal <86725015+candrahaikal@users.noreply.github.com>
2021-10-05 09:46:29 +0700

    making responsive

--- index.js
+++ index.js
@@ -12,17 +12,30 @@ const quotesList = [
 'Lelahmu belum seberapa, ini baru permulaan, ceritamu masih panjang.',
 'I’m good at lying to hide my pain.',
 'Sabar, doamu sedang bekerja, you’ll receive them at the right time.',
-'Di balik diamku ada stress yang membandel.'
+'Di balik diamku ada stress yang membandel.',
+'I’m not okay but I’m trying to.',
+'Slowly but surely, healing takes time.',
+'In the end it’s just me and myself.',
+'So fucking tired of myself.',
+'Dear self, bertahap ya, ngga semuanya harus sekarang juga, semua ada waktunya.',
+'I wish you were here, hugging me.',
+'Diterima dulu sakit pahitnya, you have a goal, you’ll get there.',
+'Take a deep breath, it’s okay, it’ll be okay.',
+'Allah tidak membebani seseorang melainkan sesuai dengan kesanggupannya. [QS. Al-Baqarah : 286]',
+'Sabar, nafas dulu sebentar, ceritamu masih panjang.',
+'Don’t stop praying, you’ll get everything you deserve.',
+'God, i’m tired, but i trust you.',
+'Kamu sedang berproses, tumbuh baik-baik ya, you’ll be the best version of you.',
+'\“I\’m so proud of you\” soon for myself.',
 ]
 
 // Rules
 btn.addEventListener('click', function(){
   let randomQuoteGenerator = Math.round(Math.random() * quotesList.length - 1);
 
-  quoteText.innerHTML = quotesList[randomQuoteGenerator];
   if(quoteText == 'undefined'){
-    console.log(randomQuoteGenerator);
+    quoteText.innerHTML = 'Tetap semangat ya!';
   } else{
-    console.log(quotesList[randomQuoteGenerator]);
+    quoteText.innerHTML = quotesList[randomQuoteGenerator];
   }
 })
