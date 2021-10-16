71a28f20a21059ce3ec15450c61f5ce9cfb7fc51
Candra Haikal <86725015+candrahaikal@users.noreply.github.com>
2021-10-16 13:39:24 +0700

    add quotes 40 -> 100

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const quoteText = document.querySelector('#main-quote');
 
 // Array quotes
 const quotesList = [
-'Sabar\, semua ada gilirannya. Mungkin bukan sekarang\, semoga nanti akan. Tetap waras ya.',
+'Sabar, semua ada gilirannya. Mungkin bukan sekarang, semoga nanti akan. Tetap waras ya.',
 'Capek, dihajar keadaan, dipersulit isi kepala sendiri, stress, hampir gila tapi gapapa.', 
 'Love yourself, you are your home, yourself is more important.',
 'Look at you, you are tired but you don\'t give up\, you are great so far.', 
@@ -43,13 +43,92 @@ const quotesList = [
 'Patahmu akan tumbuh, sedihmu akan bahagia, dan sabarmu akan terbayar.',
 'Pundakmu lelah, kuatmu hebat sejauh ini. Rehat sebentar ya, nanti kita jalan lagi pelan-pelan.',
 'No one knows berisiknya isi kepalamu, menderitanya kamu didalam dan kerasnya kamu mengusahakan yang terbaik.',
+'Someday you will find someone who hates to hurt you and feels lucky to have you.',
+'I’m still here, for you, everytime.\n-God',
+'ya Allah, kedepannya jangan berat-berat ya, ini aja udah stress hampir gila.',
+'Kuat-kuat ya, jangan nyerah dulu, nanti akan datang hari terbaikmu.',
+'Rasanya semuanya kek yaudah aja. Sedih, patah, sakit. Ya, yaudah. Capek. Hehe',
+'Moody, sensitive, overthinker, bad communication, kurang tidur, suka menyendiri, benci sepi.',
+'Tired but not giving up. I have a goal, and I will always try my best for it, for myself.',
+'I just wanna be happy again, I was so happy before.',
+'Pahit, patah, sakit, ini prosesnya, you’re on your way to be the best version of you. Kuat-kuat ya.',
+'Tuhan udah ngatur gimana baiknya. Ya berat sih, tapi gapapa, aku ngikut aja. Hehe',
+// 50 quotes
+
+'Semoga tetap waras dalam keadaan yang terkuras secara mental.',
+'I\'m tired, i\'m stressed, i\'m hurt, but i\'m okay.',
+'Pahit, patah, sakit, ini prosesnya. Keep growing huh, be stronger, you\'ll get there.',
+'Capek sama semua hal yang dipendam sendirian, i wanna tell, but i can\'t.',
+'Capek, tapi gapapa. Berat, tapi masih kuat. Huh, I\'m still trying my best.',
+'Pundaknya dikuatin lagi ya, libatkan Allah dalam segala urusanmu, Allah loves you.',
+'Sabar dulu, sabar lagi, sabar terus. It\'s okay, God has perfect timing, trust Him.',
+'Happiness and peace of mind, you deserve it.',
+'Berat ya? I know it\'s hard but please don\'t give up, be strong please, kamu hebat sampai sejauh ini.',
+'Capek, pengen cerita tapi susah buat ngejelasin apa yang lagi dirasain.',
+
+'Dear self, your pain will heal soon, I promise.',
+'Berdoa sambil nangis is the best healing.',
+'My smile is good at lying.',
+'Sabar, gapapa, mungkin bukan sekarang, tapi sedang kuusahakan.',
+'\“I\'m proud of myself\” you will get it.',
+'Aku tau ini belum seberapa, but I\'m tired.',
+'It\'s okay to feel tired, it\'s okay to cry. It hurts but it\'s okay.',
+'Capek ya? Gapapa, later you will receive what you pray for.',
+'Prosesmu berat, pundakmu dipaksa kuat. Keep growing please, you will get there.',
+'Happiness has an end, sadness too.',
+
+'I\'m not crying, but my heart.',
+'Take a deep breath. Sabar, Allah punya rencananya sendiri.',
+'Kurangin ngeluhnya, kerasin usahanya, kencengin doanya. You\'ll get your goal.',
+'Kalo mau nyerah, inget when you wanted to give up but in the end kamu bisa ngelewatin semuanya.',
+'No one knows how suffer you are inside. Be strong please.',
+'Capek banget ngga tau harus gimana lagi. Serius, capek.',
+'I know it\'s hard, kuat-kuat ya, kamu bisa kok ngelewatin semuanya.',
+'It\'s not easy but I try my best.',
+'Jangan dipaksain, it\'s ok to rest, nanti kita jalan lagi pelan-pelan.',
+'Reminder: rest is also part of the process.',
+
+'Ya mau gimana lagi, kayaknya emang udah paling bener dipendem sendiri aja.',
+'Sabar ya, kamu sedang dibentuk, nikmatin dulu pahit-pahitnya, semua ada waktunya.',
+'Kuat-kuat ya, suatu saat kamu akan menjadi seperti apa yang kamu doakan.',
+'Duduk sebentar, nafas dulu. It\'s okay, you deserve it, kamu hebat sejauh ini.',
+'Patahmu akan tumbuh, sedihmu akan bahagia, dan harapmu akan tersemogakan.',
+'Kita dipersulit isi kepala sendiri.',
+'\“Mah, pah, ambil aja nanti aku yang bayar\” one of my life goals.',
+'Kurangin ngeluhnya, sabar dulu ya, Allah tau semua tentangmu.',
+'Pundak yang kuat juga butuh istirahat. Gapapa, rehat dulu ya, you\'re great so far.',
+'Dipendem sendiri, ngga kuat lagi, nangis lagi. Capek ya.',
+
+'“When?” I don\'t know when but I believe in God\'s plan.',
+'“Kenapa harus aku?” Karena kamu mampu, Allah Mencintaimu.',
+'Ya Allah, please make it easy, pengen bahagiain kedua orang tua.',
+'Pelan-pelan, jangan dipaksain. Tenang ya, kamu bisa kok ngelewatin semuanya.',
+'Keep growing, trust the process. Ada saatnya kamu bangga karena tidak pernah menyerah.',
+'Take a deep breath, then exhale slowly. Hadeeh, berat banget ya prosesnya.',
+'Jatah gagalmu belum habis, lelahmu juga belum seberapa. Kuat-kuat ya.',
+'Akan ada saatnya “oh.. jadi ini, ternyata Tuhan baik ya, baik banget”',
+'Garis finish tiap orang itu beda-beda. Gapapa, mungkin enggak sekarang, tapi akan.',
+'Duduk sebentar, apa sih yang kamu kejar? Tarik nafas dulu, jangan terburu-buru.',
+//100 quotes
+
+'Tuhan, aku bakal baik-baik aja kan?',
+'Bagian terburuk tentang menjadi kuat adalah ga ada yang tau kalo kamu kesakitan.',
+'Jatuh sendiri, bangkit sendiri, yang nyemangatin juga diri sendiri. Gapapa, aku sayang aku',
+'Bahunya dikuatin lagi ya, semesta semakin berlebihan bercandanya',
+'Be proud of your progress, keep growing, you are great so far.',
+'Sabar ya, prosesmu juga butuh waktu. Don\'t give up please, kamu hebat sampai sejauh ini.',
+'Akan ada fase “oh.. jadi ini yang selama ini Tuhan rencanain”',
+'Hampir gila karena gatau harus gimana.',
+'Patah, sembuh, bertumbuh. Susah, tapi bisa. Lekas pulih ya.',
+'Sedih, sakit, perih. Bertahan lagi ya, harapanmumu pantas diperjuangkan.',
+
 ]
 
 // Rules
 btn.addEventListener('click', function(){
   let randomQuoteGenerator = Math.round(Math.random() * quotesList.length - 1);
 
-  if(quoteText == 'undefined'){
+  if(quoteText == undefined){
     quoteText.innerHTML = 'Tetap semangat ya!';
   } else{
     quoteText.innerHTML = quotesList[randomQuoteGenerator];
